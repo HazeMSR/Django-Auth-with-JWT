15c8b530e5c13ad5b13534ff0f12fbcc0f1ad828
HazeMSR <hazelmsr@gmail.com>
2021-11-24 22:48:16 +0000

    Travis CI

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class JobModelTests(TestCase):
+	def test_job_name(self):
+		new_job = Job(name='My job')
+		print(new_job)
+		self.assertIs(str(new_job), 'My job')
