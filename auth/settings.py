ea556c4d4315c8b193ad390a5eeb647c67929de0
HazeMSR <hazelmsr@gmail.com>
2021-11-29 20:33:04 +0000

    Travis Swagger

--- auth/settings.py
+++ auth/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
-    'rest_framework_swagger',
     'mailer',
     'users',
     'store',
